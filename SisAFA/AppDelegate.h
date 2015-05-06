96883333218556ef4e7a918ba6089ba7dc77f4ce
BryanFernandes <bryan.h.fernandes@hotmail.com>
2015-05-06 14:31:51 -0300

    Initial Commit

---
+++ SisAFA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SisAFA
+//
+//  Created by Bryan Fernandes on 06/05/15.
+//  Copyright (c) 2015 Bryan Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
